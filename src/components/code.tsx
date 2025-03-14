114ab391d153ed2673fc3e5fe802e694909169ae
brewcold <seung.yoon.yu@gmail.com>
2025-03-14 18:09:35 +0900

    fix: prevent code split for highlight.js

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,9 +1,16 @@
 'use client';
-import { useForceRender } from '../../_lib/use-force-render';
 import type { HTMLAttributes, Ref } from 'react';
 import { useEffect, forwardRef } from 'react';
 import { ExtraProps } from 'react-markdown';
 import { warning } from '../../_lib/warn';
+
+const getThemePath = (theme: string | undefined) => {
+  try {
+    return require.resolve(`highlight.js/styles/${theme || 'github'}.css`);
+  } catch (e) {
+    return require.resolve('highlight.js/styles/github.css');
+  }
+};
 
 interface MDExtraProps extends ExtraProps {
   inline?: boolean;
@@ -14,20 +21,11 @@ type Props = HTMLAttributes<HTMLElement> & MDExtraProps;
 
 export const Code = forwardRef<HTMLElement | HTMLPreElement, Props>(
   ({ inline = false, theme, ...props }: Props, ref) => {
-    const forceReRender = useForceRender();
-
     useEffect(() => {
-      (async function () {
-        await import(`highlight.js/styles/${theme || 'github'}.css`).catch(err =>
-          warning(
-            `failed to load code theme - ${theme}. To change Theme, See highlight.js`,
-            err
-          )
-        );
-      })();
-
-      forceReRender();
-    }, []);
+      import(getThemePath(theme)).catch(err =>
+        warning(`failed to load code theme - ${theme}`, err)
+      );
+    }, [theme]);
 
     if (inline) {
       return (
