2884840bbb0c564fc0ff3d99a667c962ff097359
brewcold <seung.yoon.yu@gmail.com>
2025-03-14 18:16:56 +0900

    feat: delete code themeing

--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,16 +1,7 @@
 'use client';
 import type { HTMLAttributes, Ref } from 'react';
-import { useEffect, forwardRef } from 'react';
+import { forwardRef } from 'react';
 import { ExtraProps } from 'react-markdown';
-import { warning } from '../../_lib/warn';
-
-const getThemePath = (theme: string | undefined) => {
-  try {
-    return require.resolve(`highlight.js/styles/${theme || 'github'}.css`);
-  } catch (e) {
-    return require.resolve('highlight.js/styles/github.css');
-  }
-};
 
 interface MDExtraProps extends ExtraProps {
   inline?: boolean;
@@ -21,12 +12,6 @@ type Props = HTMLAttributes<HTMLElement> & MDExtraProps;
 
 export const Code = forwardRef<HTMLElement | HTMLPreElement, Props>(
   ({ inline = false, theme, ...props }: Props, ref) => {
-    useEffect(() => {
-      import(getThemePath(theme)).catch(err =>
-        warning(`failed to load code theme - ${theme}`, err)
-      );
-    }, [theme]);
-
     if (inline) {
       return (
         <code ref={ref} className={props.className} {...props}>
