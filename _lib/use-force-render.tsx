0fdb48b0f17f4dbefaaba1ee6a1b3773a8bbdabd
vvyre <seung.yoon.yu@gmail.com>
2025-04-28 18:01:59 +0900

    chore: delete unused files and migrate

--- _lib/use-force-render.tsx
+++
@@ -1,8 +0,0 @@
-import { useCallback, useState } from 'react'
-
-export function useForceRender(): () => void {
-  const [_, setNumber] = useState<number>(0)
-  const forceRender = useCallback(() => setNumber(prev => prev + 1), [_])
-
-  return forceRender
-}
